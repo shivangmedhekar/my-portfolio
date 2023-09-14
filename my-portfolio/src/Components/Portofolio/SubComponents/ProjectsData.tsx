1a33405040fbb8b876219177f0179435b0dc9a2d
Shivang Medheakar <69140290+shivangmedhekar@users.noreply.github.com>
2023-09-14 10:07:35 -0400

    Added Projects Description

--- my-portfolio/src/Components/Portofolio/SubComponents/ProjectsData.tsx
+++ my-portfolio/src/Components/Portofolio/SubComponents/ProjectsData.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ProjectsData: {title: string, description: string}[] = [
     {title:'Intune - (Spotify x Discord)', description:"Web app enables users to listen to synchronized music in chat rooms while interacting with Spotify's extensive library and authentication features."},
-    {title:'Catch Movies in Action', description:'Website adaptable to all devices, with UI components and animated interactions.'},
-    {title:'Online store', description:'Website adaptable to all devices, with UI components and animated interactions.'}
+    {title:'Catch Movies in Action', description:'Movie booking web application, leveraging the capabilities of the AMC Developer API to provide users with a streamlined platform for browsing and reserving movie tickets.'},
+    {title:'Airline Passenger Satisfaction', description:'Built a passenger satisfaction prediction model in Python using Kaggle data, optimizing with XGBoost for a 96.14% accuracy.'}
 ];
 
 export default ProjectsData;
